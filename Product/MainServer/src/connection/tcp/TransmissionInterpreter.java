eb75146a1b6bb5fb7bb6196ee86438f3921a789f
JamesFoxes <jamescfoxes@gmail.com>
2014-10-21 11:04:59 +0200

    started main server. Making TCP connections. Next step is setting up commands.

---
+++ Product/MainServer/src/connection/tcp/TransmissionInterpreter.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection.tcp;
+
+import connection.tcp.commands.ErrorCommand;
+import connection.tcp.commands.Command;
+import java.io.IOException;
+import java.net.Socket;
+import connection.tcp.common.TransmissionPacket;
+
+/**
+ *
+ * @author JamesFoxes
+ */
+public class TransmissionInterpreter
+{
+
+    TransmissionPacket recievedTransmission;
+
+    public TransmissionInterpreter(TransmissionPacket message, Socket clientConnection)
+    {
+        recievedTransmission = message;
+        try
+        {
+            switchExecutionOnCommand().execute(clientConnection, message);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private Command switchExecutionOnCommand()
+    {
+        Command toExecute;
+        switch (recievedTransmission.command)
+        {
+            
+            default:
+                toExecute = new ErrorCommand();
+                break;
+        }
+        return toExecute;
+    }
+
+}
